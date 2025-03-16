f2f9b394b30c89b3faaf5193afd3cb1e82fb42b0
michel <michel@microu.org>
2025-03-16 18:16:08 +0100

    ...

--- src/ButtonBar.tsx
+++ src/ButtonBar.tsx
@@ -5,35 +5,37 @@ import { twMerge } from "tailwind-merge";
 
 type TProps = {
   items: ItemDef[];
-  callback?: (id: string, item:ItemData) => void;
+  callback?: (id: string, item: ItemData) => void;
   class?: string | { ul?: string; li?: string; selected?: string };
   selection?: string;
 };
 
 const baseClasses = {
-  ul: "bg-stone-200 p-2 flex flex-row flex-wrap gap-2",
+  ul: "bg-transparent p-2 flex flex-row flex-wrap gap-2",
 
-  li: "select-none cursor-pointer bg-cyan-700 text-white/90 px-1 rounded-md",
+  li: "select-none cursor-pointer bg-slate-900 text-white/90 px-1 rounded-sm",
 
-  selected: "ring-[3px] ring-offset-1 ring-red-600",
+  selected: "font-bold text-purple-400/80",
 };
 
 export function ButtonBar(props: TProps): JSX.Element {
+
+  
   const normalizedItems: () => { id: string; label: string }[] = () => {
     const r = [];
-
+    
     for (const item of props.items) {
       r.push(buildItemData(item));
     }
     return r;
   };
-
-  const callCallback = (item:ItemData) => {
+  
+  const callCallback = (item: ItemData) => {
     if (props.callback) {
       props.callback(item.id, item);
     }
   };
-
+  
   const propsClasses = () => {
     if (props.class == undefined) {
       return { ul: "", li: "", selected: "" };
@@ -43,37 +45,38 @@ export function ButtonBar(props: TProps): JSX.Element {
       return { ul: "", li: "", selected: "", ...props.class };
     }
   };
-
+  
   const ulClasses = createMemo(() => {
     const _propsClasses = propsClasses();
-
+    
     const r = twMerge(baseClasses.ul, _propsClasses.ul);
-
+    
     return r;
   });
-
+  
   const liClasses = createMemo(() => {
     const _propsClasses = propsClasses();
-
+    
     const r = twMerge(baseClasses.li, _propsClasses.li);
-
+    
     return r;
   });
-
+  
   const liSelectedClasses = createMemo(() => {
     const _propsClasses = propsClasses();
-
+    
     const r = twMerge(
       liClasses(),
-
+      
       baseClasses.selected,
-
+      
       _propsClasses.selected
     );
-
+    
     return r;
   });
-
+  
+  console.log("Create ButtonBar with items:", props.items);
   return (
     <ul class={ulClasses()}>
       <For each={normalizedItems()}>
--- src/mstyle/index.ts
+++ src/mstyle/index.ts
@@ -1,2 +1,3 @@
+export * from "./types.js";
 export * from "./mstyle.js";
-export * from "./PVStyler.js"
+export * from "./STagsStyler.js"
